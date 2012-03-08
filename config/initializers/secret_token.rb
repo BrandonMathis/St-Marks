d22afb7f9952b362d714e7678f7393c68ea80bcc
BrandonMathis <Brandon@KeysetTS.com>
2012-03-07 20:43:02 -0500

    Initial checkin of stmarks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stmarks::Application.config.secret_token = 'c1e051a93457ead781fbb515d508e937b1a95a2edb5a24ceb7a9c641f37b4742a511c5751575d8fd8150e6293b8a7f83cb63f00c1ebf4550aa5a9feaeb029b70'
