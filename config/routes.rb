583574f96d98c9896fca775c556ef6bf4f91517e
BrandonMathis <Brandon@KeysetTS.com>
2012-08-25 14:56:27 -0400

    Added high voltage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Stmarks::Application.routes.draw do
-  mount Refinery::Core::Engine, :at => '/'
+  mount Refinery::Core::Engine, :at => '/r'
+
+  root to: 'high_voltage/pages#show', id: 'front'
 end
