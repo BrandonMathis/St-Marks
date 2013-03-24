2090ec5adb02ac57167881d2ce83ea880764a00b
BrandonMathis <BeMathis@gmail.com>
2013-03-24 17:22:12 -0400

    Use sendmail on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,8 @@ Stmarks::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.delivery_method = :sendmail
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
